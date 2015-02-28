d8ea1dddd7abebd9702a1422d22930657dc3da12
davidalim <14.d.lim@gmail.com>
2015-02-28 16:17:22 -0500

    Uploading Photos

--- uploadingpictures.php
+++ uploadingpictures.php
@@ -16,6 +16,6 @@ $user = $phpFlickrObj->people_findByUsername('ph0ton1');
 $user_url = $phpFlickrObj->urls_getUserPhotos($user['id']);
 
 
-$result = $phpFlickrObj->sync_upload('images/photo1.jpg', null, null, 'mytag', 1);
-echo '<a href="http://www.google.com">google</a>';
+$result = $phpFlickrObj->sync_upload('images/photo1.jpg', 'title', 'description', 'mytag', 1);
+echo '<a href="http://www.google.com">googleeee</a>';
 ?>
