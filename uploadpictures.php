8342bc080c3b34198d86fb5ffe93cfb6dbf0967c
davidalim <14.d.lim@gmail.com>
2015-02-28 18:22:52 -0500

    Trying new key

--- uploadpictures.php
+++ uploadpictures.php
@@ -3,16 +3,15 @@
 require_once("includes/phpFlickr.php");
 
 /*-------- Flickr Authentication --------*/
-$api_key = '2da2f168f5d5d67c7532f39587cc9bdc';
-$secret = '20ebde61b471bd0e';
-$token = '72157651080024282-414b3e85b1ba9a80';
-$phpFlickrObj = new phpFlickr($api_key, $secret, true);
-$phpFlickrObj->setToken($token);
-$phpFlickrObj->auth("write");
+ $apiKey = "2da2f168f5d5d67c7532f39587cc9bdc";
+ $apiSecret = "20ebde61b471bd0e";
+ $permissions  = "write";
+ $token        = "72157651080024282-414b3e85b1ba9a80";
+
+ $f = new phpFlickr($apiKey, $apiSecret, true);
+ $f->setToken($token);
+ return $f->async_upload("photo1.jpg");
 /*---------------------------------------*/
-
-return $phpFlickrObj->async_upload("photo1.jpg");
-
 
 echo '<a href="http://www.google.com">googleeee3</a>';
 ?>
