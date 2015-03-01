215c98343641ca4fa112a96d960cf8df675c425c
davidalim <14.d.lim@gmail.com>
2015-03-01 03:28:12 -0500

    uploading works. trying to write to file

--- writetofile.php
+++ writetofile.php
@@ -1,9 +1,11 @@
 <?php 
 $filename = 'enterthevoid.pho';
-$Content = "Add this to the file\r\n";
+$Content = "Add this new to the file\r\n";
  
 echo "open";
 $handle = fopen($filename, 'x+');
+echo "clear";
+ftruncate($handle, 0);
 echo " write";
 fwrite($handle, $Content);
 echo " close";
