8e19dce28b0be9fdf3f67f1cc46a48305b11670d
davidalim <14.d.lim@gmail.com>
2015-03-01 03:36:48 -0500

    uploading works. trying to write to file

--- writetofile.php
+++ writetofile.php
@@ -5,7 +5,7 @@ $Content = "Add this new to the file\r\n";
 echo "open";
 $handle = fopen($filename, 'a') or die("can't open file");
 echo "clear";
-//ftruncate($handle, 0);
+ftruncate($handle, 0);
 echo " write";
 fwrite($handle, $Content);
 echo " close";
