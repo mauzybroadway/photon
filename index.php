87f283948ea4c85f6c200b4b504fbc727485b714
davidalim <14.d.lim@gmail.com>
2015-03-01 09:16:57 -0500

    GOOD VERSION

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 include_once ("home.html");
 
-echo '<div id="content">';
-echo '<div class="images">';
-$count = 1;
-while ($count<20) {
-
-	echo'<img src="images/photo' . $count . '.jpg" class="thumb"/>';
-	$count += 1;
-}
-
-echo '</div>';
-echo '</div>';
+header('Location: index.php');	
 
 ?>
